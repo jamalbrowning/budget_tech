79bb1d8f98029fcaf75eaacbf139996cd5c06110
Jim Browning <jamalebrowning@gmail.com>
2021-11-11 15:35:02 -0600

    wip

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-import time
-
 import redis
-from flask import Flask
+from flask import Flask,request, jsonify
+import os
 
-app = Flask(__name__)
+# Init app
+def create_app():
+  app = Flask(__name__)
+  return app
+
+app = create_app()
+basedir = os.path.abspath(os.path.dirname(__file__))
 cache = redis.Redis(host='redis', port=6379)
 
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+#database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-    count = get_hit_count()
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+
+    return jsonify({'msg':'hello world'})
